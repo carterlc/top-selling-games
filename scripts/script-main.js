07346bbde7483bc4179c6d95cd08c030716f6e66
Carter Clifford <carterlc@live.ca>
2023-02-04 15:55:58 -0600

    Top ten, Top ten NA EU, and Top 1000 graphs all show now. NA and EU buttons dont work now

--- scripts/script-main.js
+++ scripts/script-main.js
@@ -1,12 +1,19 @@
 import data from '../json/video_games.json' assert { type: 'json' };
-const game_data = data.video_games;
+const game_data = data[0].data;
 
-
+console.log(game_data);
 export function getAllSales() {
     let games = [];
-    for(i = 0; i < game_data.length; i++) {
+    for(let i = 0; i < game_data.length; i++) {
         games.push(game_data[i].Global_Sales);
-        console.log(game_data[i].Global_Sales)
+    }
+    return games;
+}
+
+export function getAllGames() {
+    let games = [];
+    for(let i = 0; i < game_data.length; i++) {
+        games.push(game_data[i].Name);
     }
     return games;
 }
